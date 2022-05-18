cf7e26a8eed4aa5a0edc571e067b00f81c516c1a
Luiz Felipe <luizfpereira406@gmail.com>
2022-05-18 15:06:11 -0300

    Requisito 1

---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        Pagina de Edição do Perfil
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
