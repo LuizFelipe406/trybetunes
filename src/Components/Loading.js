6f9788cbbcba8205f5bc61522086bbf385e72a54
Luiz Felipe <luizfpereira406@gmail.com>
2022-05-18 17:25:47 -0300

    Estilização pagina de login

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 class Loading extends React.Component {
   render() {
+    const { classe } = this.props;
     return (
-      <p>
+      <p className={ classe }>
         Carregando...
       </p>
     );
   }
 }
+
+Loading.propTypes = {
+  classe: propTypes.string.isRequired,
+};
 
 export default Loading;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 import Loading from '../Components/Loading';
+import '../css/Login.css';
 
 class Login extends React.Component {
   state = {
@@ -32,26 +33,33 @@ class Login extends React.Component {
     const { userName, loading, redirect } = this.state;
     const minLength = 3;
     const form = (
-      <form>
-        <input
-          type="text"
-          placeholder="Digite seu nome"
-          data-testid="login-name-input"
-          onChange={ this.handleChange }
-        />
-        <button
-          type="button"
-          data-testid="login-submit-button"
-          disabled={ userName.length < minLength }
-          onClick={ this.onClickButtonEntrar }
-        >
-          Entrar
-        </button>
-      </form>
+      <div className="login-page">
+        <div className="titles">
+          <span className="title-1">Trybe</span>
+          <span className="title-2">Tunes</span>
+        </div>
+        <form>
+          <input
+            type="text"
+            placeholder="Digite seu nome"
+            data-testid="login-name-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            style={ { backgroundColor: userName.length < minLength ? 'red' : 'blue' } }
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ userName.length < minLength }
+            onClick={ this.onClickButtonEntrar }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
     return (
       <div data-testid="page-login">
-        {loading ? <Loading /> : form}
+        {loading ? <Loading classe="loading-login-page" /> : form}
         {redirect && <Redirect to="/search" />}
       </div>
     );
